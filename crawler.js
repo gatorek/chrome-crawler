c2f9a76e63b3f7515b57c3cbae99456e0e607882
Michał Sacewicz <michal@sacewicz.pl>
2017-10-04 09:17:49 +0200

    cli options

--- crawler.js
+++ crawler.js
@@ -1,16 +1,28 @@
 var Crawler = require("simplecrawler");
 const CDP = require('chrome-remote-interface');
 const chromeLauncher = require('chrome-launcher');
+const commandLineArgs = require('command-line-args')
 
+const optionDefinitions = [
+  { name: 'max-depth', alias: 'd', type: Number, defaultOption: 1 },
+  { name: 'max-concurrency', alias: 'c', type: Number, defaultOption: 10 },
+  { name: 'time-interval', alias: 't', type: Number, defaultOption: 100 },
+  { name: 'url', alias: 'u', type: String}
+]
+const options = commandLineArgs(optionDefinitions)
+if (! /^(http|https):\/\/[a-zA-Z0-9]+\.[a-zA-Z0-9]+/.test(options.url) ){
+    console.error('Option -u|--url must be set and be valid URL');
+    process.exit(1);
+}
 
-var crawler = new Crawler("http://URL/");
-crawler.interval = 100; // Ten seconds 
-crawler.maxConcurrency = 10;
-crawler.maxDepth = 2; // First page and discovered links from it are fetched 
+console.log(options)
+
+var crawler = new Crawler(options.url);
+crawler.interval = options["time-interval"];
+crawler.maxConcurrency = options["max-concurrency"];
+crawler.maxDepth = options["max-depth"]; 
 crawler.downloadUnsupported = false;
 
-var debugPort = 9222;
-var gChrome;
 
 crawler.on("fetchcomplete", function(queueItem, responseBuffer, response) {
     if (/^text\/html/.test(response.headers['content-type'])) {
@@ -34,14 +46,14 @@ crawler.on("fetchcomplete", function(queueItem, responseBuffer, response) {
                 }).catch((err) => {
                     console.error(err);
                     client.close();
-                });
+                })
             }).on('error', (err) => {
                 // cannot connect to the remote endpoint
                 console.error(err);
             }).on('complete', function() {
                 console.log('CRAWL complete');
                 chrome.kill();
-            });
+            })
         }).then(chrome => {
             console.log('CRAWL complete');
             chrome.kill();
