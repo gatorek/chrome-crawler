1e75021776d85dfd27a2e1e3a47acee641670f99
Michał Sacewicz <michal@sacewicz.pl>
2017-10-05 17:07:27 +0200

    porządki, dodatkowe opcje CLI

--- crawler.js
+++ crawler.js
@@ -1,82 +1,88 @@
-var Crawler = require("simplecrawler");
-const CDP = require('chrome-remote-interface');
-const chromeLauncher = require('chrome-launcher');
+var Crawler = require("simplecrawler")
+const CDP = require('chrome-remote-interface')
+const chromeLauncher = require('chrome-launcher')
 const commandLineArgs = require('command-line-args')
+var __ = require('underscore')
+var kue = require('kue'),
+    queue = kue.createQueue()
 
 const optionDefinitions = [
-  { name: 'max-depth', alias: 'd', type: Number, defaultOption: 1 },
-  { name: 'max-concurrency', alias: 'c', type: Number, defaultOption: 10 },
-  { name: 'time-interval', alias: 't', type: Number, defaultOption: 100 },
+  { name: 'max-depth', alias: 'd', type: Number, defaultValue: 1 },
+  { name: 'max-concurrency', alias: 'c', type: Number, defaultValue: 10 },
+  { name: 'max-chrome-concurrency', alias: 'g', type: Number, defaultValue: 10 },
+  { name: 'time-interval', alias: 't', type: Number, defaultValue: 100 },
+  { name: 'help', alias: 'h', type: Boolean },
   { name: 'url', alias: 'u', type: String}
 ]
 const options = commandLineArgs(optionDefinitions)
-if (! /^(http|https):\/\/[a-zA-Z0-9]+\.[a-zA-Z0-9]+/.test(options.url) ){
-    console.error('Option -u|--url must be set and be valid URL');
-    process.exit(1);
+if(options.help || ! /^(http|https):\/\/[a-zA-Z0-9]+\.[a-zA-Z0-9]+/.test(options.url) ) {
+    console.error(`Usage: node crawler.js -u URL [options]
+    -u, --url                     url
+    -d, --max-depth               recursion level
+    -c, --max-concurrency         crawler concurrency
+    -g, --max-chrome-concurrency  browser concurrency
+    -t, --time-interval           crawler time interval
+    -h, --help                    this help
+    `)
+    process.exit(0)
 }
 
-console.log(options)
-
-var crawler = new Crawler(options.url);
-crawler.interval = options["time-interval"];
-crawler.maxConcurrency = options["max-concurrency"];
-crawler.maxDepth = options["max-depth"]; 
-crawler.downloadUnsupported = false;
-
-
+// Setup crawler
+var crawler = new Crawler(options.url)
+crawler.interval = options["time-interval"]
+crawler.maxConcurrency = options["max-concurrency"]
+crawler.maxDepth = options["max-depth"]
+crawler.downloadUnsupported = false
 crawler.on("fetchcomplete", function(queueItem, responseBuffer, response) {
     if (/^text\/html/.test(response.headers['content-type'])) {
-        launchChrome().then(chrome => {
-            CDP({ port: chrome.port }, (client) => {
-                // extract domains
-                const { Network, Page } = client;
-                // setup handlers
-                Network.requestWillBeSent((params) => {
-                    console.log('URL: ' + queueItem.url + " | " + params.request.url);
-                });
-                Page.loadEventFired(() => {
-                    client.close();
-                });
-                // enable events then start!
-                Promise.all([
-                    Network.enable(),
-                    Page.enable()
-                ]).then(() => {
-                    return Page.navigate({ url: queueItem.url });
-                }).catch((err) => {
-                    console.error(err);
-                    client.close();
-                })
-            }).on('error', (err) => {
-                // cannot connect to the remote endpoint
-                console.error(err);
-            }).on('complete', function() {
-                console.log('CRAWL complete');
-                chrome.kill();
-            })
-        }).then(chrome => {
-            console.log('CRAWL complete');
-            chrome.kill();
-        });
+        console.log('CRAWL: ' + queueItem.url)
+        queue.create('url', {url: queueItem.url}).removeOnComplete(true).save()
     }
-});
+})
 
 function launchChrome(headless = true) {
     return chromeLauncher.launch({
-        // port: 9222, // Uncomment to force a specific port of your choice.
         chromeFlags: [
             '--window-size=412,732',
             '--disable-gpu',
             headless ? '--headless' : ''
         ]
-    });
+    })
 }
 
-crawler.start();
+crawler.start()
 
-// launchChrome().then(chrome => {
-//   console.log(`Chrome debuggable on port: ${chrome.port}`);
-//   debugPort = chrome.port;
-//   gChrome = chrome;
-//   crawler.start();
-// });
+// Start workers
+__.times(options["max-chrome-concurrency"], chromeId => {
+    launchChrome().then(chrome => {
+        queue.process('url', (job, done) => {
+            CDP({ port: chrome.port }, (client) => {
+                // extract domains 
+                const { Network, Page } = client
+                // setup handlers
+                Network.requestWillBeSent((params) => {
+                    console.log('URL: ' + job.data.url + " | " + params.request.url)
+                })
+                Page.loadEventFired(() => {
+                    client.close()
+                    done()
+                })
+                // enable events then start!
+                Promise.all([
+                    Network.enable(),
+                    Page.enable()
+                ]).then(() => {
+                    console.log('NAV: browser: ' + chromeId + ' url: ' + job.data.url)
+                    return Page.navigate({ url: job.data.url })
+                }).catch((err) => {
+                    console.error('Error in browser: ' + chromeId + ', URL: ' + job.data.url)
+                    console.error(err)
+                    client.close()
+                })
+            }).on('error', (err) => {
+                console.error('Error in browser: ' + chromeId + ', URL: ' + job.data.url)
+                console.error(err)
+            })
+        })
+    })
+})
